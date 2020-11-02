37b2bf68448f7ffc0402af2d371ddd831cd8a5b7
Valentin Caraba <if18b122@technikum-wien.at>
2020-11-02 12:49:16 +0100

    Tcp Listener created and Request Context class added

---
+++ RestHttpWebService/NUnitTests/RequestContextTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using RestHttpWebService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    class RequestContextTest
+    {
+        //arrange
+        RequestContext context = new RequestContext();
+        //string testdata = "POST /messages/1 HTTP/1.1\nHOST: LOCALHOST:13000\nUSER-AGENT: CURL/7.55.1\nACCEPT: */*\nCONTENT-LENGTH: 23\nCONTENT-TYPE: APPLICATION/X-WWW-FORM-URLENCODED\n\nthe payload is here";
+
+        [Test]
+        [Category("pass")]
+        [TestCase("POST /messages/1 HTTP/1.1\nHOST: LOCALHOST:13000\nUSER-AGENT: CURL/7.55.1\nACCEPT: */*\nCONTENT-LENGTH: 23\nCONTENT-TYPE: APPLICATION/X-WWW-FORM-URLENCODED\n\nthe payload is here")]
+        public void ReadContextTest(string data)
+        {
+            //act
+            context.ReadContext(data);
+            //verify
+            Assert.AreEqual("POST", context.httpVerb);
+            Assert.AreEqual("messages", context.dirName);
+            Assert.AreEqual("1", context.resourceID);
+            Assert.AreEqual("HTTP/1.1", context.httpVersion);
+            Assert.AreEqual("the payload is here", context.payload);
+        }
+    }
+}
---
+++ RestHttpWebService/RestHttpWebService/RequestContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestHttpWebService
+{
+    public class RequestContext
+    {
+        public string httpVerb;
+        public string dirName;
+        public string resourceID;
+        public string httpVersion;
+        public string payload;
+
+        public RequestContext() { }
+
+        public void ReadContext(string data)
+        {
+
+            this.httpVerb = data.Substring(0, data.IndexOf("/") - 1);
+            //s1 string from dir name onwards POST /message/1 HTTP/1.1 --> message/1 HTTP/1.1
+            string s1 = data.Substring(data.IndexOf("/") + 1);
+            this.httpVersion = s1.Substring(s1.IndexOf("HTTP"), 8);
+            //messages/1 HTTP/1.1 --> messages/1
+            s1 = s1.Substring(0, s1.IndexOf(" "));
+            if(s1.IndexOf("/") == -1)
+            {
+                this.dirName = s1;
+                this.resourceID = null;
+            }else
+            {
+                this.dirName = s1.Substring(0, s1.IndexOf("/"));
+                this.resourceID = s1.Substring(s1.IndexOf("/") + 1);
+            }
+            string[] lines = data.Split(new string[] { "\n" }, StringSplitOptions.None);
+            
+            this.payload = lines[7];
+        }
+    }
+}
