79684b227b801caf7deaafdca23c537445ab656e
Valentin Caraba <if18b122@technikum-wien.at>
2020-11-12 18:01:15 +0100

    initial commit testing branch

--- RestHttpWebService/NUnitTests/RequestContextTest.cs
+++ RestHttpWebService/NUnitTests/RequestContextTest.cs
@@ -11,17 +11,16 @@ namespace NUnitTests
     [TestFixture]
     class RequestContextTest
     {
-        //arrange
-        RequestContext context = new RequestContext();
-
         [Test]
         [Category("pass")]
         [TestCase("POST /messages/1 HTTP/1.1\r\nHOST: LOCALHOST:13000\r\nUSER-AGENT: CURL/7.55.1\r\nACCEPT: */*\r\nCONTENT-LENGTH: 23\r\nCONTENT-TYPE: APPLICATION/X-WWW-FORM-URLENCODED\r\n\r\nthe payload is here")]
         public void ReadContextTestPost(string data)
         {
+            //arrange
+            RequestContext context = new RequestContext();
             //act
             context.ReadContext(data);
-            //verify
+            //assert
             Assert.AreEqual("POST", context.httpVerb);
             Assert.AreEqual("messages", context.dirName);
             Assert.AreEqual("1", context.resourceID);
@@ -31,12 +30,19 @@ namespace NUnitTests
 
         [Test]
         [Category("pass")]
-        [TestCase("GET /messages/1 HTTP/1.1\r\nHOST: LOCALHOST:13000\r\nUSER-AGENT: CURL/7.55.1\r\nACCEPT: */*\r\nCONTENT-LENGTH: 23\r\nCONTENT-TYPE: APPLICATION/X-WWW-FORM-URLENCODED\r\n\r\n")]
+        [TestCase("GET /messages/1 HTTP/1.1\r\n" +
+                  "HOST: LOCALHOST:13000\r\n" +
+                  "USER-AGENT: CURL/7.55.1\r\n" +
+                  "ACCEPT: */*\r\n" +
+                  "CONTENT-LENGTH: 23\r\n" +
+                  "CONTENT-TYPE: APPLICATION/X-WWW-FORM-URLENCODED\r\n\r\n")]
         public void ReadContextTestGet(string data)
         {
+            //arrange
+            RequestContext context = new RequestContext();
             //act
             context.ReadContext(data);
-            //verify
+            //assert
             Assert.AreEqual("GET", context.httpVerb);
             Assert.AreEqual("messages", context.dirName);
             Assert.AreEqual("1", context.resourceID);
